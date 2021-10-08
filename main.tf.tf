d1c4198b52dc499817d3582d0d3bb6344428dce5
sachinmane83 <81758592+sachinmane83@users.noreply.github.com>
2021-10-08 15:32:21 +0530

    Update main.tf.tf

--- main.tf.tf
+++ main.tf.tf
@@ -5,20 +5,51 @@ provider "google" {
 
 }
 
+variable "config" {
+  type = map(any)
+  default = {
+    prod = "n1-standard-1"
+    dev = "f1-micro"
+  }
+}
+variable "env" {}
+#variable "apps" {
+#  type = list(any)
+#  default = ["app1", "app2", "app3"]
+
+locals {
+
+    mt = var.env == "prod" ? var.config["prod"] : var.config["dev"]
+
+}
+
+// Virual machine for app1
+
 resource "google_compute_instance" "vm_instance" {
-      name         = "gcptutorials-vm1"
-      machine_type = "f1-micro"
-    
-      boot_disk {
-        initialize_params {
-          image = "debian-cloud/debian-9"
-        }
-      }    
-      network_interface {       
-        network = "default"
-        access_config {
-        }
-      }
+
+  #  count = length(var.apps)
+
+  #  name = "myvm-${var.env}-${count.index + 1}" //  myvm-prod-1, myvm=prod-2, myvm-dev-1, myvm-dev-2
+
+  name         = "myvm-${var.env}"
+  machine_type = local.mt
+
+  boot_disk {
+
+    initialize_params {
+
+      image = "debian-cloud/debian-9"
+
     }
-    
+
+  }
+
+  network_interface {
+
+    network = "default"
+
+    access_config {}
+
+  }
+}
   
