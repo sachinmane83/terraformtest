69cfdd412f1c8ad672cf20ffc2935b56ed719080
sachinmane83 <81758592+sachinmane83@users.noreply.github.com>
2021-10-08 15:22:32 +0530

    Update main.tf.tf

--- main.tf.tf
+++ main.tf.tf
@@ -6,7 +6,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-      name         = "gcptutorials-vm"
+      name         = "gcptutorials-vm1"
       machine_type = "f1-micro"
     
       boot_disk {
